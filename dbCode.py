cdb9252d800cb99c2ccbc91853857a0a3e49c5b3
Sathyanarayanan G <nsathya33@gmail.com>
2020-10-10 18:57:26 +0530

    Everything else.
    All the remaining files have been committed, with functionalities missing.

--- dbCode.py
+++ dbCode.py
@@ -1,6 +1,6 @@
 import mysql.connector
 from werkzeug import generate_password_hash, check_password_hash
-def loginverify(user,pwd):
+def loginVerify(user,pwd):
     mydb = mysql.connector.connect(
       host="localhost",
       user="sathya",
@@ -9,12 +9,12 @@ def loginverify(user,pwd):
     )
     mycursor = mydb.cursor()
     sql = "SELECT * FROM userdata WHERE mail_id = %s"
-    adr = (user, )
+    adr = (user,)
     mycursor.execute(sql, adr)
     myresult = mycursor.fetchone()
     if myresult:
       if(check_password_hash(myresult[4],pwd)):
-        return [myresult[1],myresult[0]]
+        return [myresult[1],myresult[0],myresult[5]]
       else:
         return [0]
     else:
@@ -34,7 +34,7 @@ def addUser(name,mail,phn,pwd,role):
     mycursor.execute(sql, adr)
     mydb.commit()
 
-def populateClasses(uid):
+def studentPopulateClasses(uid):
   mydb = mysql.connector.connect(
       host="localhost",
       user="sathya",
@@ -45,5 +45,86 @@ def populateClasses(uid):
   sql = "select classid,classname from classrooms where classid in (select classID from studentclass where userid="+str(uid)+");"
   mycursor.execute(sql)
   myresult=mycursor.fetchall()
+  return myresult
+
+def ownerPopulateClasses(uid):
+  mydb = mysql.connector.connect(
+      host="localhost",
+      user="sathya",
+      passwd="password",
+      database="classroomdb"
+    )
+  mycursor = mydb.cursor()
+  sql = "select classid,classname from classrooms where ownerid="+str(uid)+";"
+  mycursor.execute(sql)
+  myresult=mycursor.fetchall()
+  return myresult
+
+def retrieveClassData(classid):
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="sathya",
+      passwd="password",
+      database="classroomdb"
+    )
+    mycursor = mydb.cursor()
+    sql = "select * from materials where classid ="+classid+";"
+    mycursor.execute(sql)
+    myresult=mycursor.fetchall()
+    return myresult
+
+def addMaterial(classid,mname,ownerid,url,m_type):
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="sathya",
+      passwd="password",
+      database="classroomdb"
+    )
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO materials (mname,url,classid,ownerid,m_type) values (%s,%s,%s,%s,%s);"
+    adr = (name, mail, phn, generate_password_hash(pwd), role)
+    print(adr)
+    mycursor.execute(sql, adr)
+    mydb.commit()
+
+def addClass(className,ownerId):
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="sathya",
+      passwd="password",
+      database="classroomdb"
+    )
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO classrooms (ClassName,Ownerid) values (%s,%s);"
+    adr = (className,ownerId)
+    mycursor.execute(sql, adr)
+    mydb.commit()
+
+def joinClassroom(classid,userid):
+    mydb = mysql.connector.connect(
+      host="localhost",
+      user="sathya",
+      passwd="password",
+      database="classroomdb"
+    )
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO studentclass (userid,classid) values (%s,%s);"
+    adr = (userid,classid)
+    print(adr)
+    mycursor.execute(sql, adr)
+    mydb.commit()
+
+def allUsers():
+  mydb = mysql.connector.connect(
+      host="localhost",
+      user="sathya",
+      passwd="password",
+      database="classroomdb"
+    )
+  mycursor = mydb.cursor()
+  sql = "select * from userdata;"
+  mycursor.execute(sql)
+  myresult=mycursor.fetchall()
+  print(myresult)
   return myresult
 
--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template,request,redirect,url_for,flash,session,g
-from dbCode import loginverify,addUser,populateClasses
+from dbCode import loginVerify,addUser,studentPopulateClasses,ownerPopulateClasses,retrieveClassData,addClass,joinClassroom,allUsers
 
 app = Flask(__name__)
 app.secret_key = 'somesecretkeythatonlyishouldknow'
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
 
 @app.before_request
 def before_request():
@@ -19,8 +21,7 @@ def login():
         print("SumbissionLogDebug")
         user=request.form['user']
         pwd=request.form['pwd']
-        token=loginverify(user,pwd)
-        print(token[0])
+        token=loginVerify(user,pwd)
         if(token[0]==0):
             return render_template('login.html',message="Invalid password")
         elif(token[0]==-1):
@@ -28,8 +29,9 @@ def login():
         else:
             session['user_id']=token[1]
             session['user_name']=token[0]
+            session['user_role']=token[2]
+            print(session['user_role'])
             return redirect(url_for('dashboard'))
-        
         
 
 @app.route('/signup',methods=['GET','POST'])
@@ -45,18 +47,95 @@ def signup():
         addUser(name,email,phone,pwd,role)
         return redirect(url_for('login'))
 
+
 @app.route('/dashboard',methods=['GET','POST'])
 def dashboard():
     if not g.user:
         return redirect(url_for('login'))
     if request.method == 'GET':
-        classes=populateClasses(session['user_id'])
-        return render_template('dashboard.html',classes=classes)
+        if(session['user_role']=="3"):
+            classes=studentPopulateClasses(session['user_id'])
+            return render_template('studentDashboard.html',classes=classes)
+        elif(session['user_role']=="2"):
+            classes=ownerPopulateClasses(session['user_id'])
+            return render_template('ownerDashboard.html',classes=classes)
+        elif(session['user_role']=="1"):
+            return render_template('adminDashboard.html')
+
 
 @app.route('/logout',methods=['GET','POST'])
 def logout():
     session.pop('user_id',None)
     return redirect(url_for('login'))
+
+
+@app.route('/classStream',methods=['GET','POST'])
+def classStream():
+    if not g.user:
+        return redirect(url_for('login'))
+    if request.method == 'POST':
+        classid=request.form['goclass']
+        if(session['user_role']=="3"):
+            classes=retrieveClassData(classid)
+            print("Student")
+            return render_template('studentClassStream.html',classes=classes)
+        elif(session['user_role']=="Owner"):
+            classes=retrieveClassData(classid)
+            return render_template('ownerClassStream.html',classes=classes)
+        return render_template('classStream.html')
+
+
+@app.route('/ownerClasses',methods=['GET','POST'])
+def ownerClasses():
+    if not g.user:
+        return redirect(url_for('login'))
+    if request.method == 'GET':
+        return render_template('ownerClasses.html')
+
+
+@app.route('/newClass',methods=['GET','POST'])
+def newClass():
+    if not g.user:
+        return redirect(url_for('login'))
+    if request.method == 'GET':
+        return render_template('newClass.html')
+    elif request.method == 'POST':
+        className = request.form['classname']
+        addClass(className,session['user_id'])
+        return redirect(url_for('dashboard'))
+
+@app.route('/joinClass',methods=['GET','POST'])
+def joinClass():
+    if not g.user:
+        return redirect(url_for('login'))
+    if request.method == 'GET':
+        return render_template('joinClass.html')
+    elif request.method == 'POST':
+        classid = request.form['classname']
+        joinClassroom(classid,session['user_id'])
+        return redirect(url_for('dashboard'))
+
+@app.route('/deleteUser',methods=['GET','POST'])
+def deleteUser():
+    if not g.user:
+        return redirect(url_for('login'))
+    if request.method == 'GET':
+        users=allUsers()
+        return render_template('deleteUser.html',users=users)
+    """elif request.method == 'POST':
+        classid = request.form['classname']
+        joinClassroom(classid,session['user_id'])
+        return redirect(url_for('dashboard'))"""
+
+@app.route('/upload/',methods = ['GET','POST'])
+def upload_file():
+    if request.method =='POST':
+        file = request.files['file']
+        if file:
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            return hello()
+    return render_template('file_upload.html')
     
 
 if __name__ == '__main__':
